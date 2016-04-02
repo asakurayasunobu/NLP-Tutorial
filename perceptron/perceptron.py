454a77e4b6d285a05eabe85f2b7494624324a75e
asakurayasunobu <tennisabc562@gmail.com>
2016-04-02 20:38:26 +0900

    4/2

---
+++ perceptron/perceptron.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+
+from collections import defaultdict
+
+def create_features(x):
+    phi = defaultdict(lambda:0)
+    words = x.strip().split()
+    for word in words:
+        phi['UNI:{}'.format(word)] += 1
+    return phi
+
+def predict_one(weight, phi):
+    score = 0
+    for word, freq in phi.items():
+        if word in weight:
+            score += freq * weight[word]
+    if score >= 1:
+        return 1
+    else:
+        return -1
+
+
+def update_weights(weight, phi, y):
+    for word, freq in phi.items():
+        weight[word] += freq*y
+
+
+def predict_all(weight, test_file):
+    for line in test_file:
+        phi = create_features(line.strip())
+        pred_y = predict_one(weight, phi)
+        print(pred_y)
+
+
+def main():
+    train_file = open('../nlptutorial/data/titles-en-train.labeled')
+    test_file = open('../nlptutorial/data/titles-en-test.word')
+    #--train--
+    iterations = range(10)
+    weight = defaultdict(lambda:0)
+    for i in iterations:
+        for line in train_file:
+            y, x = line.strip().split('\t')
+            phi = create_features(x)
+            pred_y = predict_one(weight, phi)
+            if pred_y != y:
+                update_weights(weight,phi,int(y))
+    #--test--
+    predict_all(weight, test_file)
+
+if __name__ == '__main__':
+    main()
